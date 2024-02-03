750461ddd068e920fe8f9f9208356fe466bfcd7f
Mayur-Palavalli <mayur.palavalli@gmail.com>
2024-02-03 15:46:26 -0800

    Individual Club Screen + Event Screen

---
+++ app/src/main/java/com/example/clubsapp2/Event1Page.java
@@ -0,0 +1,14 @@
+package com.example.clubsapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Event1Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_page);
+    }
+}
