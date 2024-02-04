84bc4d1e47a79e34fef76d439fcd9a307cea8555
iluusions <ssidharth.tho@gmail.com>
2024-02-03 18:23:17 -0800

    Merging of Mayur + Sid changes

--- app/src/main/java/com/example/schooltime/MainActivity.java
+++ app/src/main/java/com/example/schooltime/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         clubacc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, clubacc.class);
+                Intent intent = new Intent(MainActivity.this, login.class);
                 startActivity(intent);
             }
         });
