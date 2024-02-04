527bbf16ad7fc67cbaa6473654aabbab2e1d5954
iluusions <ssidharth.tho@gmail.com>
2024-02-03 19:32:58 -0800

    Final commit

---
+++ app/src/main/java/com/example/schooltime/unfinished.java
@@ -0,0 +1,14 @@
+package com.example.schooltime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class unfinished extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.unfinished);
+    }
+}
